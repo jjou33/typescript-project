05fa315051646221b8bc87980d354d30f7e75abf
happycho <zanda33@naver.com>
2022-07-08 13:28:50 +0900

    docs: Typescript 기본 개념 및 활용 예시

---
+++ class-note/3_interface.ts
@@ -0,0 +1,77 @@
+// 인터페이스 정의
+interface User {
+    age: number;
+    name: string;
+}
+
+// 변수에 활용한 인터페이스
+let test: User = {
+    age: 33,
+    name: '테스트'
+}
+
+// 함수에 활용한 인터페이스
+
+function getUser(user: User) {
+    console.log(user);
+}
+
+const param = {
+    age: 11,
+    name: '캡틴'
+}
+
+getUser(param);
+
+// 함수의 스펙(구조)에 인터페이스 활용
+
+interface SumFunc {
+    (a: number, b:number): number;
+}
+
+let sum: SumFunc;
+sum = function (a: number, b:number) {
+    return a+b
+}
+
+// 인덱싱
+
+interface StringArray {
+    [index: number]: string;
+}
+
+let arr: StringArray = ['a', 'b']
+// arr[0] = 10
+
+// 딕셔너리 패턴
+
+interface StringRegexDictionary {
+    [key: string]: RegExp // 정규표현식 예약어
+}
+
+let obj: StringRegexDictionary= {
+    // sth: /abc/,
+    cssFile: /\.css$/,
+    jsFile: /\.js$/
+}
+
+Object.keys(obj).forEach(function(value) {
+    
+})
+
+// 인터페이스 확장
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface Developer extends Person{
+    language: string;
+}
+
+let hw: Developer = {
+    language: 'hi',
+    name: 'hel',
+    age: 10
+}
