0b2363be7472da49830f9a1ef6ee31b8e57a2e10
happycho <zanda33@naver.com>
2022-07-14 16:54:01 +0900

    address-book-project-index.ts 타입 정의

--- quiz-project/2_address-book/src/index.ts
+++ quiz-project/2_address-book/src/index.ts
@@ -10,11 +10,17 @@ interface Contact {
   phones: PhoneNumberDictionary;
 }
 
+enum PhoneType {
+  Home = 'home',
+  Office = 'office',
+  Studio = 'studio',
+}
+
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
-function fetchContacts() {
+function fetchContacts(): Promise<Contact[]> {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  const contacts = [
+  const contacts: Contact[] = [
     {
       name: 'Tony',
       address: 'Malibu',
@@ -57,42 +63,52 @@ function fetchContacts() {
 // main
 class AddressBook {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  contacts = [];
+  contacts: Contact[] = [];
 
   constructor() {
     this.fetchData();
   }
 
-  fetchData() {
+  fetchData(): void {
     fetchContacts().then(response => {
-      // this.contacts = response;
+      this.contacts = response;
     });
   }
 
   /* TODO: 아래 함수들의 파라미터 타입과 반환 타입을 지정해보세요 */
-  findContactByName(name) {
+  findContactByName(name: string): Contact[] {
     return this.contacts.filter(contact => contact.name === name);
   }
 
-  findContactByAddress(address) {
+  findContactByAddress(address: string): Contact[] {
     return this.contacts.filter(contact => contact.address === address);
   }
 
-  findContactByPhone(phoneNumber, phoneType: string) {
+  // 여기에서 phoenType 을 그냥  String 으로 갈경우 사용자가 보낼때 오기입을 할 수 있다.
+  // 예를들어, findContactByPhone('homee'); 또는 findContactByPhone('officee');
+  // 위와 같이 잘못보낼 여지가 있지만 타입을 enum 으로 선언하고 사용하게되면
+  // 아래와 같이 자동완성도 가능하고 오류를 예방할 수 있다.
+  // findContactByAddress(PhoneType.Home)
+  // enum PhoneType {
+  //   Home = 'home',
+  //   Office = 'office',
+  //   Studio = 'studio',
+  // }
+  findContactByPhone(phoneNumber: number, phoneType: PhoneType): Contact[] {
     return this.contacts.filter(
       contact => contact.phones[phoneType].num === phoneNumber
     );
   }
 
-  addContact(contact) {
+  addContact(contact: Contact): void {
     this.contacts.push(contact);
   }
 
-  displayListByName() {
+  displayListByName(): string[] {
     return this.contacts.map(contact => contact.name);
   }
 
-  displayListByAddress() {
+  displayListByAddress(): string[] {
     return this.contacts.map(contact => contact.address);
   }
   /* ------------------------------------------------ */
